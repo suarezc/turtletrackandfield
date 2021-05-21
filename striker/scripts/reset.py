3f36e2aa691fba56566f7ef0d563813713d526ad
Jonah Arthur Kaye <jonahkaye@machoke.cs.uchicago.edu>
2021-05-20 21:52:17 -0500

    yaw?

--- striker/scripts/reset.py
+++ striker/scripts/reset.py
@@ -1,16 +1,17 @@
 import rospy
-from gazebo_msgs.msg import ModelState
+from gazebo_msgs.msg import ModelState, ModelStates
 from geometry_msgs.msg import Pose, Twist, Point, Quaternion, Vector3
 
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
 
-
-class GazeboTools:
+class GazeboTools(object):
 
   def __init__(self):
+
     rospy.init_node("reset_world")
 
     self.set_model_state = rospy.Publisher("/gazebo/set_model_state", ModelState, queue_size=10)
+    rospy.Subscriber("gazebo/model_states", ModelStates, self.model_states_received)
 
     self.pins = {
           "pin1": [3, 0, 0],
@@ -24,12 +25,35 @@ class GazeboTools:
           "pin9": [3.520, 0.1, 0],
           "pin10": [3.520, 0.3, 0],
           "ball": [1, 0, 0],
-        #  "TODO figure out robot name": [0, 0, 0],
+        #  "TODO figure out robot name": [0, 0, 0]
         }
+    self.current_numbered_blocks_locations = None
+    self.reward = 0 
 
-  def model_state_received(self, data):
+  def model_states_received(self, data):
     # TODO, compute score, check if it should be reset
-    pass
+    #print(data)
+    if (self.current_numbered_blocks_locations == None):
+      self.current_numbered_blocks_locations = {}
+    for pin_name,pose in self.pins.items():
+      block_idx = data.name.index(pin_name)
+      self.current_numbered_blocks_locations[pin_name] = data.pose[block_idx].orientation
+      theta = self.current_numbered_blocks_locations[pin_name]
+      yaw = (euler_from_quaternion([
+            theta.x,
+            theta.y,
+            theta.z,
+            theta.w])
+            [2])
+      print(yaw)
+    print("____________")
+    rospy.sleep(2)
+      #if z > 0.3:
+        #self.reward += 1
+    
+
+
+
 
   def reset_world(self):
     q_list = quaternion_from_euler(0, 0, 0)
@@ -54,3 +78,11 @@ class GazeboTools:
 
     for i in range(20):
       self.reset_world()
+
+  def run(self):
+          rospy.spin()
+
+if __name__=="__main__":
+
+    node = GazeboTools()
+    node.run()
